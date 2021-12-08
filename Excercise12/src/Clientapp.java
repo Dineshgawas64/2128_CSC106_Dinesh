c2e127a55ecba673727bf67af24daafe3b819276
Dineshgawas64 <dineshgawas64@gmail.com>
2021-12-08 19:58:28 +0530

    updated

--- Excercise12/src/Clientapp.java
+++ Excercise12/src/Clientapp.java
@@ -6,15 +6,26 @@ public class Clientapp {
 
 		public static void main(String[] args) {
 			// TODO Auto-generated method stub
-			Queue obj = new Queue();
-			obj.enqueue("a");
-			obj.enqueue("b");
-			obj.enqueue("c");
-			obj.enqueue("d");
-			obj.dequeue();
-			obj.peek();
-			obj.display();
-			System.out.println(obj);
-		}
+		Queue q = new Queue();
+		
+		q.enqueue("a");
+		q.enqueue("b");
+		q.enqueue("c");
+		
+		System.out.println(q.getQueuelist());
+		
+		q.dequeue();
+		
+		System.out.println(q.getQueuelist());
+		
+		
+			
+		System.out.print("first element - ");  
+		q.peek();
+		
+	//	System.out.print("number of elements - ");  
+	//	q.count();
+			
+		}			
 
 }
--- Excercise12/src/com/Dinesh/csc106/ex12/Queue.java
+++ Excercise12/src/com/Dinesh/csc106/ex12/Queue.java
@@ -1,74 +1,69 @@
 package com.Dinesh.csc106.ex12;
 
-import java.util.List;
 import java.util.ArrayList;
-
+import java.util.List;
 
 public class Queue {
 	
-	private List<String> queueList;
-	Integer count;
+	private List<String> queuelist;
+
 	
+
+//	public Queue() {
+//		
+//	}
+
+
 	public Queue() {
-		
-		this.queueList = new ArrayList<String>();
-	}
-	public boolean isEmpty() {
-		return this.count == 0;
-		
-	}
-	
-	
-	
-	public void enqueue(String person) {
-		
-		queueList.add(person);
-		count++;
-		System.out.println("Added\n");
-		
-	}
-	
-	
-	
-	public String dequeue() {
-		
-		if(isEmpty()) {
-			return null;
-		}
-		 count--;
-		return queueList.remove(0);
-   
-	}
-	
-	
-	
-	public String peek() {
-		
-		if(isEmpty()) {
-			return null;
-		}
-		return this.queueList.get(0);
-	}
-	
-	
-	
-	
-	public void display() {
-		
-		for(String item : this.queueList) {
-			System.out.println(item);
-		}
-	}
-	
-	
-
-	public List<String> getQueueList() {
-		return queueList;
+		this.queuelist = new ArrayList<String>();
 	}
 
-	public void setQueueList(List<String> queueList) {
-		this.queueList = queueList;
+
+	public void enqueue(String name) {
+		
+		queuelist.add(name);
+		
+	}
+	
+	public void dequeue() {
+		
+	System.out.println(queuelist.remove(0));	
+	
+		
+	}
+	
+	
+//	public void count() {
+//		
+//		
+//	}
+//	
+	
+	public void peek() {
+		
+		System.out.println(queuelist.get(0));
+
+		
+	
+	}
+	
+	public List getQueuelist() {
+		return queuelist;
 	}
 
+	public void setQueuelist(List queuelist) {
+		this.queuelist = queuelist;
+	}
+
+
+	public void count() {
+		System.out.println(queuelist.size());
+		
+	}
+
+	
+	
+	
+	
 	
 }
